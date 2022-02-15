090a1cbc81c58f4d921b6706fdbbd43b2c05ddb3
Artem Stepanenko <98549196+Pipssec@users.noreply.github.com>
2022-02-15 19:52:40 +0300

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -11,15 +11,18 @@ for p in range(1, 6):
     films = soup.findAll('div', class_='desktop-rating-selection-film-item')
     for film in films:
         link = "https://www.kinopoisk.ru/" + film.find('a', class_='selection-film-item-meta__link').get('href')
-        russian_name = film.find('a', class_='selection-film-item-meta__link').find('p',
-                                                                                    class_='selection-film-item-meta__name').text
-        original_name = film.find('a', class_='selection-film-item-meta__link').find('p',
-                                                                                     class_='selection-film-item-meta__original-name').text
-        country = film.find('a', class_='selection-film-item-meta__link').find('span',
-                                                                               class_='selection-film-item-meta__meta-additional-item').text
-        film_type = film.find('a', class_='selection-film-item-meta__link').findAll('span',
-                                                                                    class_='selection-film-item-meta__meta-additional-item').text
-        rate = film.find('span', class_='rating__value.rating__value_positive').text
+        russian_name = film.find('a', class_='selection-film-item-meta__link').find('p', class_='selection-film-item-meta__name').text
+        original_name = film.find('a', class_='selection-film-item-meta__link').find('p', class_='selection-film-item-meta__original-name').text
+        country = film.find('a', class_='selection-film-item-meta__link').find('span', class_='selection-film-item-meta__meta-additional-item').text
+        try:
+            film_type = film.find('a', class_='selection-film-item-meta__link').findAll('span', class_="selection-film-item-meta__meta-additional-item").text
+        except:
+            film_type = '-'
+        try:
+            rate = film.find('span', class_='rating__value.rating__value_positive').text
+        except:
+            rate = '-'
+
 
         data.append([link, russian_name, original_name, country, film_type, rate])
 
